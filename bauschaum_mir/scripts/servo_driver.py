eda150479848445d0a65b8d061d71722eaab8fbc
matchRos <47657974+matchRos@users.noreply.github.com>
2022-03-28 16:57:40 +0200

    Add files via upload

--- bauschaum_mir/scripts/servo_driver.py
+++ bauschaum_mir/scripts/servo_driver.py
@@ -3,11 +3,18 @@
 import rospy
 from dynamixel_workbench_msgs.srv import DynamixelCommand
 from std_msgs.msg import Int16
-
+import subprocess
 
 class servo_driver():
 
     def __init__(self):
+        # give serial read/write permission 
+        bashCommand = "sudo chmod 666 /dev/ttyUSB0"
+        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+        print(output)
+        ###
+        
         rospy.init_node("servo_driver_node")
         service_topic = "/dynamixel_workbench/dynamixel_command"
         rospy.wait_for_service(service_topic)
