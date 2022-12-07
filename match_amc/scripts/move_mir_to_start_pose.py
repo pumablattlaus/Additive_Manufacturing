c7219f248892417c55e5d59ba3a7d60bc5404347
matchRos <Recker@match.uni-hannover.de>
2022-12-07 15:13:16 +0100

    added action client to move to start pose

--- match_amc/scripts/move_mir_to_start_pose.py
+++ match_amc/scripts/move_mir_to_start_pose.py
@@ -3,11 +3,11 @@ import rospy
 import tf
 from tf import transformations
 from nav_msgs.msg import Path, Odometry
-from geometry_msgs.msg import Twist,  Pose
+from geometry_msgs.msg import Twist,  Pose, PoseStamped
 from custom_path import MyTrajectory
 import math
-
-from match_lib.match_robots import MirNav2Goal
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal 
+import actionlib
 
 class move_mir_to_start_pose():
 
@@ -15,6 +15,7 @@ class move_mir_to_start_pose():
     def __init__(self):
         rospy.init_node("move_mir_to_start_pose_node")
         self.config()
+        self.mir_start_pose = PoseStamped()
 
     def run(self):
         # wait for all trajectories to be received
@@ -26,18 +27,29 @@ class move_mir_to_start_pose():
         rospy.sleep(1)
         # Move to initial pose
         # Because first goal of self.target_trajectory is not reached by cartesain formation controller
-        mir=MirNav2Goal("/mur216")
-        first_pose = Pose()
-        first_pose.position.x = self.target_trajectory.x[0]
-        first_pose.position.y = self.target_trajectory.y[0]
-        first_pose.orientation.w = 1.0
-        mir.sendGoalPos(first_pose)
+        self.mir_start_pose.header.frame_id = "map"
+        self.mir_start_pose.pose.position.x = self.target_trajectory.x[0]
+        self.mir_start_pose.pose.position.y = self.target_trajectory.y[0]
+        q = transformations.quaternion_from_euler(0,0,self.target_trajectory.phi[0])
+        self.mir_start_pose.pose.orientation.x = q[0]
+        self.mir_start_pose.pose.orientation.y = q[1]
+        self.mir_start_pose.pose.orientation.z = q[2]
+        self.mir_start_pose.pose.orientation.w = q[3]
         while not rospy.is_shutdown():
-            res = mir.is_ready()
-            if res:
+            self.move_to_goal(self.mir_start_pose)
+            rospy.sleep(0.1)
+            if self.reached_goal == True:
                 break
-            rospy.sleep(0.5)
-        rospy.logdebug("MiR at goal")
+            else:
+                rospy.logerr("could not reach initial pose. Retrying...")  
+
+        # wait for mir to reach goal
+        while not rospy.is_shutdown():
+            distance_to_goal = math.sqrt((self.mir_pose.position.x - self.target_trajectory.x[0])**2 + (self.mir_pose.position.y - self.target_trajectory.y[0])**2)
+            if distance_to_goal < 0.1:
+                rospy.loginfo("mir reached initial pose")
+                break
+            rospy.sleep(0.1)
            
         # set correct orientation
         rospy.loginfo("setting correct orientation")
@@ -58,7 +70,7 @@ class move_mir_to_start_pose():
                 break
             self.cmd_vel_publisher.publish(self.robot_command)
         
-            rospy.loginfo("turnung",e_phi)
+            rospy.loginfo("turning")
 
 
         rospy.set_param("/mir_initialized",True)
@@ -86,11 +98,27 @@ class move_mir_to_start_pose():
     def mir_pose_cb(self,msg=Odometry()):
         self.mir_pose = msg.pose.pose
 
+    def move_to_goal(self,pose=PoseStamped()):
+        client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+        rospy.loginfo("Waiting for move base server")
+        client.wait_for_server()
+
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = pose.header.frame_id
+        goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.pose = pose.pose
+
+        client.send_goal(goal)
+        client.wait_for_result()
+        return client.get_result()
+
+
     def config(self):
         self.control_rate = rospy.get_param("~control_rate")
         self.mir_target_trajectory_topic = rospy.get_param("~mir_target_trajectory_topic")
         self.mir_pose_topic = rospy.get_param("~mir_pose_topic")
         self.mir_cmd_vel_topic = rospy.get_param("~mir_cmd_vel_topic")
+        self.move_base_simple_goal_topic = rospy.get_param("~move_base_simple_goal_topic")
         self.mir_pose = Pose()
         self.cmd_vel_publishers = []
         self.robot_command = Twist()
@@ -108,6 +136,10 @@ class move_mir_to_start_pose():
         rospy.Subscriber(self.mir_target_trajectory_topic, Path, self.target_trajectory_cb)
         rospy.Subscriber(self.mir_pose_topic, Odometry, self.mir_pose_cb)
         self.cmd_vel_publisher = rospy.Publisher(self.mir_cmd_vel_topic, Twist, queue_size=1)
+        self.simple_goal_publisher = rospy.Publisher(self.move_base_simple_goal_topic, PoseStamped, queue_size=1)
+
+
+
 
 
 if __name__=="__main__":
