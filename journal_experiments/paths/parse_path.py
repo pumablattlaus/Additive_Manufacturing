337c6236638602aa834f31c38a6685301ca2c365
YassineMch <yassine.mechri@stu.uni-hannover.de>
2023-04-21 13:55:04 +0200

    fixed small issue

--- journal_experiments/paths/parse_path.py
+++ journal_experiments/paths/parse_path.py
@@ -30,16 +30,16 @@ ur_pub = rospy.Publisher("/ur_path", Path, queue_size= 1)
 
 mir_path = Path()
 mir_path_point = PoseStamped()
-mir_path.header.frame_id = "mocap"
+mir_path.header.frame_id = "map"
 mir_path.header.stamp = rospy.Time.now()
 ur_path = Path()
 ur_path_point = PoseStamped()
-ur_path.header.frame_id = "mocap"
+ur_path.header.frame_id = "map"
 ur_path.header.stamp = rospy.Time.now()
 
 
-mir_path.poses = [PoseStamped() for i in range(len(mir_x))] 
-ur_path.poses = [PoseStamped() for i in range(len(wall_x))] 
+mir_path.poses = [PoseStamped() for i in range(len(mir_x)-1)] 
+ur_path.poses = [PoseStamped() for i in range(len(wall_x)-1)] 
 
 # reverse mir path
 # for i in range(0,len(mir_x)):
@@ -58,6 +58,12 @@ ur_path.poses = [PoseStamped() for i in range(len(wall_x))]
 for i in range(0,len(mir_x)-1): #len(robot0_xhat)
     mir_path_point.pose.position.x = mir_x[i]
     mir_path_point.pose.position.y = mir_y[i]
+
+    if mir_x[i] == 0.0 or mir_y[i] == 0.0:
+        print("mir_path_point.pose.position.x = 0.0")
+
+    mir_path_point.header.frame_id = "map"
+    mir_path_point.header.stamp = rospy.Time.now()
 
     # add orientation
     orientation = math.atan2(mir_y[i+1]-mir_y[i], mir_x[i+1]-mir_x[i])
@@ -86,8 +92,8 @@ for i in range(0,len(wall_x)-1): #len(robot0_xhat)
 
 rospy.sleep(2)
 mir_pub.publish(mir_path)
-rospy.sleep(2)
-ur_pub.publish(ur_path)
-rospy.sleep(1)
+rospy.sleep(3)
+#ur_pub.publish(ur_path)
+rospy.sleep(3)
 
-rospy.spin()
+
