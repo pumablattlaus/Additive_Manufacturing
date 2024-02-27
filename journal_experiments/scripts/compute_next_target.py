c8e94deaedd37e83b1c4616bdce4c24991014d8e
Tobias Recker <Recker@match.uni-hannover.de>
2024-02-27 16:09:00 +0100

    v0.1

--- journal_experiments/scripts/compute_next_target.py
+++ journal_experiments/scripts/compute_next_target.py
@@ -11,6 +11,7 @@ import math
 class ComputeNextTarget():
 
     def config(self):
+        self.control_rate = 100
         self.ur_target_velocity = 0.1
 
     def __init__(self):
@@ -27,6 +28,8 @@ class ComputeNextTarget():
         self.mir_path_index = 0
         self.ur_path_index = 0
 
+        self.ur_distance_travelled = 0.0
+        self.mir_distance_travelled = 0.0
 
 
         self.tf_broadcaster = TransformBroadcaster()
@@ -42,23 +45,26 @@ class ComputeNextTarget():
         rospy.wait_for_message("/ur_path", Path)
         rospy.loginfo("Received mir and ur path")
 
+        # check length of mir and ur path
+        self.check_path_length()
+
         # set current mir and ur pose to first pose in path
         self.current_mir_pose = self.mir_target_path.poses[0]
         self.current_ur_pose = self.ur_target_path.poses[0]
 
         # compute distance travelled by mir and ur
-        self.ur_distance_travelled = self.compute_distance_travelled(self.ur_target_path)
-        self.mir_distance_travelled = self.compute_distance_travelled(self.mir_target_path)
+        self.ur_path_distances = self.compute_distance_travelled(self.ur_target_path)
+        self.mir_path_distances = self.compute_distance_travelled(self.mir_target_path)
 
-        print(self.ur_distance_travelled)
+        print("ur path distances: " + str(self.ur_path_distances))
 
-        #self.compute_next_target()
+        rate = rospy.Rate(self.control_rate)
+        while not rospy.is_shutdown():
+            self.compute_next_target()
+            rate.sleep()
 
 
     def compute_next_target(self):
-        # check length of mir and ur path
-        self.check_path_length()
-
         # get mir and ur target based on path index
         self.get_target_from_path()
 
@@ -71,23 +77,33 @@ class ComputeNextTarget():
         # broadcast next target
         self.broadcast_next_target()
 
+        # update distance travelled by ur and mir
+        self.update_distance_travelled()
+
+    def update_distance_travelled(self):
+        self.mir_distance_travelled += self.mir_target_velocity / self.control_rate
+        self.ur_distance_travelled += self.ur_target_velocity / self.control_rate
+
+
     def compute_distance_travelled(self, path):
         distance = []
         for i in range(len(path.poses)-1):
-            distance.append(math.sqrt((path.poses[i].pose.position.x - path.poses[i+1].pose.position.x)**2 + (path.poses[i].pose.position.y - path.poses[i+1].pose.position.y)**2))
+            distance.append(math.sqrt((path.poses[i].pose.position.x - path.poses[i+1].pose.position.x)**2 + (path.poses[i].pose.position.y - path.poses[i+1].pose.position.y)**2) + distance[i-1] if i > 0 else 0.0)
         return distance
 
 
     def check_target_reached(self):
-        pass
+        if self.ur_path_distances[self.ur_path_index] <= self.ur_distance_travelled + self.ur_target_velocity / self.control_rate:
+            self.ur_path_index += 1
 
+        #print("ur path distance: " + str(self.ur_path_distances[self.ur_path_index]) + " ur distance travelled: " + str(self.ur_distance_travelled) )
 
     def get_target_from_path(self):
         self.mir_target = self.mir_target_path.poses[self.mir_path_index]
         self.ur_target = self.ur_target_path.poses[self.ur_path_index]
 
     def compute_mir_velocity(self):
-        pass
+        self.mir_target_velocity = 0.1
     
     def check_path_length(self):
         if len(self.mir_target_path.poses) != len(self.ur_target_path.poses):
@@ -97,19 +113,16 @@ class ComputeNextTarget():
             rospy.loginfo("Length of mir and ur path match and is: " + str(len(self.mir_target_path.poses)))
 
     def broadcast_next_target(self):
-        rate = rospy.Rate(10)
-        for i in range(len(self.mir_target_path.poses)):
-            self.tf_broadcaster.sendTransform((self.mir_target_path.poses[i].pose.position.x, self.mir_target_path.poses[i].pose.position.y, self.mir_target_path.poses[i].pose.position.z),
-                                              (self.mir_target_path.poses[i].pose.orientation.x, self.mir_target_path.poses[i].pose.orientation.y, self.mir_target_path.poses[i].pose.orientation.z, self.mir_target_path.poses[i].pose.orientation.w),
-                                              rospy.Time.now(),
-                                              "mir_target",
-                                              "map")
-            self.tf_broadcaster.sendTransform((self.ur_target_path.poses[i].pose.position.x, self.ur_target_path.poses[i].pose.position.y, self.ur_target_path.poses[i].pose.position.z),
-                                              (self.ur_target_path.poses[i].pose.orientation.x, self.ur_target_path.poses[i].pose.orientation.y, self.ur_target_path.poses[i].pose.orientation.z, self.ur_target_path.poses[i].pose.orientation.w),
-                                              rospy.Time.now(),
-                                              "ur_target",
-                                              "map")
-            rate.sleep()
+        self.tf_broadcaster.sendTransform((self.ur_target.pose.position.x, self.ur_target.pose.position.y, self.ur_target.pose.position.z),
+                                        (self.ur_target.pose.orientation.x, self.ur_target.pose.orientation.y, self.ur_target.pose.orientation.z, self.ur_target.pose.orientation.w),
+                                        rospy.Time.now(),
+                                        "next_ur_target",
+                                        "map")
+        self.tf_broadcaster.sendTransform((self.mir_target.pose.position.x, self.mir_target.pose.position.y, self.mir_target.pose.position.z),
+                                        (self.mir_target.pose.orientation.x, self.mir_target.pose.orientation.y, self.mir_target.pose.orientation.z, self.mir_target.pose.orientation.w),
+                                        rospy.Time.now(),
+                                        "next_mir_target",
+                                        "map")
 
     def mir_path_callback(self, path):
         self.mir_target_path = path
